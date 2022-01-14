6ce4f356dd4f9e0a14ce65d4a4d93dad0e4534f7
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2022-01-14 15:18:17 +0530

    Update test_cb.py

--- python/tests/test_cb.py
+++ python/tests/test_cb.py
@@ -90,7 +90,7 @@ def helper_getting_started_example(which_cb):
         for j, i in zip(actual, output):
             print(i)
             print(j)
-            self.assertAlmostEqual(i, j, 0.0002, "line mismatch should be: " + j + " output: " + i)
+            assert np.isclose(i, j, atol=1e-4) #"line mismatch should be: " + j + " output: " + i)
 
 def test_getting_started_example_with():
     train_df, test_df = helper_get_data()
